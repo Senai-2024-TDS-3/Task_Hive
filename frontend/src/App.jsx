54160ffd8aae6f6f3a53dcb6d724976cd8db4284
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-26 16:23:24 -0300

    att user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import Teste from './pages/Teste'
 import Home from './Home'
+
 function App() {
 	return (
 		<>
-			<Home />
+			<Teste />
 		</>
 	)
 }
--- frontend/src/pages/User_Start.jsx
+++ frontend/src/pages/User_Start.jsx
@@ -1,13 +1,21 @@
 import User_Navbar from "../components/User_Navbar";
 import User_Gerenciar_Task from "../components/User_Gerenciar_Task"
 import Hive_Layout_Users from "../components/Hive_Layout_Users";
+import Cadastrar_User from "../components/Cadastrar_User";
+import Form_Cadastro_User from "../components/Form_Cadastro_User";
+import Hive_Card_Login from "../components/Hive_Card_Login";
+import Admin_NavBar from "../components/Admin_Navbar"
 
 export default function User_Start() {
     return (
         <div>
-            <User_Navbar />
+             {/* <Admin_NavBar/> */}
+            {/* <User_Navbar /> */}
            {/* <User_Gerenciar_Task/> */}
-           / <Hive_Layout_Users/>
+            {/* <Cadastrar_User/>  */}
+           {/* <Form_Cadastro_User/> */}
+           {/* <Hive_Card_Login/>  */}
+           < Hive_Layout_Users/>
         </div>
     )
 }
