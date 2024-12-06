067c57c3d292cbe5cc2664d7563933f7980c5f80
unknown <felipengueira1@gmail.com>
2024-12-06 15:56:17 -0300

    Componentes deletados

--- frontend/src/components/Hex_Teste.jsx
+++
@@ -1,9 +0,0 @@
-export default function Hex_Teste() {
-    return (
-        <div className="hex_wrapper">
-            <div className="hex_teste">
-                <h3><strong>TESTE</strong></h3>
-            </div>
-        </div>
-    )
-}
